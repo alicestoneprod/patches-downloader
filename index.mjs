61b23785baf2054e3350144ed7b83497fa846a53
Alice Morozova <alicemorozovafront@gmail.com>
2024-10-09 00:48:41 +0300

    added https protocol support

--- index.mjs
+++ index.mjs
@@ -1,57 +1,82 @@
 import fs from "fs"
 import http from "http"
+import https from "https"
 import { error } from "./error.mjs"
 import cliProgress from "cli-progress"
 
 const sizeFormatter = (bytes) => {
   const sizeInMB = bytes / (1024 * 1024)
-  return Math.round(sizeInMB * 100) / 100 // Round to two decimal places
+  return Math.round(sizeInMB * 100) / 100
+}
+
+const getProtocol = (link) => {
+  return link.includes("https") ? https : link.includes("http") ? http : null
 }
 
 const downloadFile = (outputPath, fileName, link, retries = 3) => {
   return new Promise((resolve, reject) => {
     const file = fs.createWriteStream(outputPath + fileName)
-    const request = http.get(link, (response) => {
-      if (response.statusCode !== 200) {
-        file.close()
-        fs.unlink(outputPath + fileName, () => {}) // Delete the file async
-        return reject(`Failed to get '${link}' (${response.statusCode})`)
-      }
+    const protocol = getProtocol(link)
+    if (protocol) {
+      const request = protocol.get(link, (response) => {
+        if (response.statusCode !== 200) {
+          file.close()
+          fs.unlink(outputPath + fileName, () => {})
+          return reject(`Failed to get '${link}' (${response.statusCode})`)
+        }
 
-      const totalSize = parseInt(response.headers["content-length"], 10)
-      let downloadedSize = 0
+        const totalSize = parseInt(response.headers["content-length"], 10)
+        let downloadedSize = 0
 
-      console.log(`\nDownloading file: ${fileName}...`)
+        console.log(`\nDownloading file: ${fileName}...`)
 
-      const bar = new cliProgress.SingleBar({
-        format: "{bar} {percentage}% | {value}/{total} MB | Speed: {speed} KB/s",
-        barCompleteChar: "\u2588",
-        barIncompleteChar: "\u2591",
-        hideCursor: true,
-      })
+        const bar = new cliProgress.SingleBar({
+          format: "{bar} {percentage}% | {value}/{total} MB | Speed: {speed} KB/s",
+          barCompleteChar: "\u2588",
+          barIncompleteChar: "\u2591",
+          hideCursor: true,
+        })
 
-      bar.start(sizeFormatter(totalSize), 0, {
-        speed: "N/A",
-      })
+        bar.start(sizeFormatter(totalSize), 0, {
+          speed: "N/A",
+        })
 
-      response.on("data", (chunk) => {
-        file.write(chunk)
-        downloadedSize += chunk.length
-        const speed = (downloadedSize / ((Date.now() - startTime) / 1000) / 1024).toFixed(2)
-        bar.update(sizeFormatter(downloadedSize), {
-          speed,
-          value: (downloadedSize / 1024 / 1024).toFixed(2),
+        response.on("data", (chunk) => {
+          file.write(chunk)
+          downloadedSize += chunk.length
+          const speed = (downloadedSize / ((Date.now() - startTime) / 1000) / 1024).toFixed(2)
+          bar.update(sizeFormatter(downloadedSize), {
+            speed,
+            value: (downloadedSize / 1024 / 1024).toFixed(2),
+          })
+        })
+
+        response.on("end", () => {
+          file.end()
+          bar.stop()
+          console.log(`File ${fileName} downloaded!\n`)
+          resolve()
+        })
+
+        response.on("error", (err) => {
+          fs.unlink(outputPath + fileName, () => {
+            if (retries > 0) {
+              console.log(`Retrying download: ${fileName}... (${retries} retries left)`)
+              resolve(downloadFile(outputPath, fileName, link, retries - 1))
+            } else {
+              fs.appendFile(
+                "logs.txt",
+                `Error while reading config file: ${fileName} - ${err}\n
+                `,
+              )
+              resolve()
+            }
+          })
         })
       })
 
-      response.on("end", () => {
-        file.end()
-        bar.stop()
-        console.log(`File ${fileName} downloaded!\n`)
-        resolve()
-      })
-
-      response.on("error", (err) => {
+      request.on("error", (err) => {
+        file.close()
         fs.unlink(outputPath + fileName, () => {
           if (retries > 0) {
             console.log(`Retrying download: ${fileName}... (${retries} retries left)`)
@@ -59,33 +84,19 @@ const downloadFile = (outputPath, fileName, link, retries = 3) => {
           } else {
             fs.appendFile(
               "logs.txt",
-              `Error while reading config file: ${fileName} - ${err}\n
+              `Error while reading config file: ${fileName} - ${err}
               `,
             )
             resolve()
           }
         })
       })
-    })
 
-    request.on("error", (err) => {
-      file.close()
-      fs.unlink(outputPath + fileName, () => {
-        if (retries > 0) {
-          console.log(`Retrying download: ${fileName}... (${retries} retries left)`)
-          resolve(downloadFile(outputPath, fileName, link, retries - 1))
-        } else {
-          fs.appendFile(
-            "logs.txt",
-            `Error while reading config file: ${fileName} - ${err}
-            `,
-          )
-          resolve()
-        }
-      })
-    })
-
-    const startTime = Date.now()
+      const startTime = Date.now()
+    } else {
+      console.error("Protocol error. Protocol http / https not found in link.")
+      throw new Error()
+    }
   }).catch((msg) => {
     console.log(msg + " , skipping...")
   })
